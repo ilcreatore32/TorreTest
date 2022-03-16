1e64fe0741443759134bc3706b24fe85408baf01
Weishler <ilcreatore321@gmail.com>
2022-03-16 18:20:13 -0400

    Change vite base

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/bio": {
-        target: "https://torre.bio/api/bios",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/bio/, ""),
-      },
-    },
-  },
+  base: "/TorreTest",
   plugins: [react()],
 });
